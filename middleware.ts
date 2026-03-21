18ab3519b724cc7d2f91403cbf573887dc83010b
Stella <stella@heybit.io>
2026-03-21 17:09:50 +0900

    로그인 가드 제거

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: [
-    // 보호할 경로 지정
-    "/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)",
-  ],
+  // matcher: [
+  //   // 보호할 경로 지정
+  //   "/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)",
+  // ],
+  matcher: [],
 };
